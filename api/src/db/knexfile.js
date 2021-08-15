812bcfa1d4cd737904a778b5b0d70b4d88f68aae
Souhaila Benallal <Souhaila.ben.allal@student.ehb.be>
2021-08-15 21:36:32 +0200

    Edit database

--- api/src/db/knexfile.js
+++ api/src/db/knexfile.js
@@ -1,37 +1,18 @@
-// Update with your config settings.
-
 module.exports = {
-
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: 'dev_5',
-      user:     'postgres',
-      password: 'postgres'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'dev_5',
-      user:     'postgres',
-      password: 'postgres'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  config: {
+      client: 'postgresql',
+      connection: {
+          host:'db',// write host:'db' if works with docker and 127.0.0.1 without docker
+          database: 'dev_5',
+          user: 'postgres',
+          password: 'postgres'
+      },
+      pool: {
+          min: 2,
+          max: 10
+      },
+      migrations: {
+          tableName: 'knex_migrations'
+      }
   }
-
 };
